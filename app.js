2bd2241775e5e4051fb1f586fa6e0327a620bf37
ashishpatel46 <ashishkp421@gmail.com>
2025-11-11 15:50:40 +0530

    Add welcome route for URL Shortener API

--- app.js
+++ app.js
@@ -38,4 +38,9 @@ app.listen(3000,()=>{
     console.log("Server is running on http://localhost:3000");
 })
 
+app.get("/",(req, res)=>{
+   return res.send("Welcome to URL Shortener API");
+})
+
+
 // GET - Redirection 
