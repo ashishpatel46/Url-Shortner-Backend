78d649bcbb6f7c541f693db7375d1884b8736477
ashishpatel46 <ashishkp421@gmail.com>
2025-11-17 17:31:10 +0530

    Enhance MongoDB connection handling in config
    Refactor MongoDB connection logic to track connection state and improve error handling.

--- src/config/monogo.config.js
+++ src/config/monogo.config.js
@@ -1,16 +1,22 @@
 import mongoose from "mongoose";
-console.log(process.env.MONGO_URI);
+
+let isConnected = false; 
 
 const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+
+    isConnected = conn.connections[0].readyState;
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
   }
 };
 
