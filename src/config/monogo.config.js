6209018221bfb22c2fae286e33a17c2fcc29ae5e
ashishpatel46 <ashishkp421@gmail.com>
2026-04-22 22:42:35 +0530

    Comment out original MongoDB connection code
    Comment out the original MongoDB connection code in monogo.config.js.

--- src/config/monogo.config.js
+++ src/config/monogo.config.js
@@ -30,3 +30,28 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
+
+// import mongoose from "mongoose";   //original code
+
+// let isConnected = false; 
+
+// const connectDB = async () => {
+//   if (isConnected) {
+//     return;
+//   }
+
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+
+//     isConnected = conn.connections[0].readyState;
+//     console.log("MongoDB connected");
+//   } catch (err) {
+//     console.error("MongoDB connection error:", err);
+//   }
+// };
+
+// export default connectDB;
