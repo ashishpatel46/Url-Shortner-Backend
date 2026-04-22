aa5e8be34a5f5b1ddf50e26e120476ceeeb68503
ashishpatel46 <ashishkp421@gmail.com>
2026-04-22 12:31:42 +0530

    Refactor MongoDB connection logic with caching

--- src/config/monogo.config.js
+++ src/config/monogo.config.js
@@ -1,23 +1,32 @@
 import mongoose from "mongoose";
 
-let isConnected = false; 
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error("Please define MONGO_URI");
+}
+
+// Global cache 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  if (isConnected) {
-    return;
+  if (cached.conn) {
+    return cached.conn; //  reuse existing connection
   }
 
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => {
+      console.log("MongoDB Connected");
+      return mongoose;
     });
-
-    isConnected = conn.connections[0].readyState;
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default connectDB;
